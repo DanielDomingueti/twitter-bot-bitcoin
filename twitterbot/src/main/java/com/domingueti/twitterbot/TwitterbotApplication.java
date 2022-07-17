14afd9c66fac33082fbce5e60316dd0fd29f4ad9
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-07-17 11:10:37 -0300

    feat: created

---
+++ twitterbot/src/main/java/com/domingueti/twitterbot/TwitterbotApplication.java
@@ -0,0 +1,13 @@
+package com.domingueti.twitterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterbotApplication.class, args);
+	}
+
+}
