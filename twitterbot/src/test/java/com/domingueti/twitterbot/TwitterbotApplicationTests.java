899ed1a41d22ea2052eda8d46679924be015833c
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-07-17 11:10:37 -0300

    feat: created

---
+++ twitterbot/src/test/java/com/domingueti/twitterbot/TwitterbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domingueti.twitterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
