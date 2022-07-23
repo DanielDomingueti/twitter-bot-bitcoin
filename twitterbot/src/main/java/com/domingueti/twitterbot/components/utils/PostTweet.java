d3e1524d6303e53381a1f1b1f9347f4fe8ed1d80
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-07-23 07:02:44 -0300

    feat: UTC time

--- twitterbot/src/main/java/com/domingueti/twitterbot/components/utils/PostTweet.java
+++ twitterbot/src/main/java/com/domingueti/twitterbot/components/utils/PostTweet.java
@@ -1,5 +1,10 @@
 package com.domingueti.twitterbot.components.utils;
 
+import java.text.DateFormatSymbols;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+
 import com.domingueti.twitterbot.module.crypto.dtos.response.CryptoDTO;
 
 import twitter4j.Twitter;
@@ -14,14 +19,19 @@ public class PostTweet {
 	    	
 	    	TwitterFactory tf = new TwitterFactory();
 		    Twitter twitter = tf.getInstance();	
+
+		    //Calculate current UTC
+			String instantStr = Instant.now().truncatedTo(ChronoUnit.MILLIS).toString().replaceAll("[TZ]", " ");
+			String date = instantStr.substring(0, 10);
+			String hour = instantStr.substring(11, 19);
+			String month = new DateFormatSymbols(Locale.US).getMonths()[Integer.valueOf(date.substring(5, 7))-1];
+			String day = date.substring(8, 10);
+			String finalStr = "\uD83D\uDD50[" + month + " " + day + "] " + hour;
 		    
 		    String changeIcon = "\uD83D\uDD3B"; //decrease standard code;
-
 		    if (dto.getHasIncreased()) {
 		    	changeIcon = "\u2B06"; //increase code 
 		    }
-		    
-		    //add time UTC twice a day 
 		    
 		    String response = 
 		    		" $" + dto.getSymbol() + " " +
@@ -29,6 +39,7 @@ public class PostTweet {
 		    		"\n\uD83D\uDCB5 Price $" + dto.getPriceUsd() +
 		    		"\n" + changeIcon + dto.getStatus() + ": " +
 		    		dto.getChangeValue() + "% in the last 24h" +
+		    		"\n" + finalStr +
 		    		"\n\n#" + dto.getSymbol() + " #crypto #java #code";
 		    
 		    twitter.updateStatus(response);
